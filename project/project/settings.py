61083c7f5f3eb6336665bc5635b206f9bb860220
Ron Cohen <ron@opbeat.com>
2013-04-27 19:29:19 +0200

    Heroku settings.

--- project/project/settings.py
+++ project/project/settings.py
@@ -168,3 +168,11 @@ LOGIN_REDIRECT_URL = '/'
 
 GITHUB_APP_ID = os.environ.get('GITHUB_APP_ID')
 GITHUB_API_SECRET = os.environ.get('GITHUB_APP_SECRET')
+
+# Heroku stuff
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
