e7bb3d567913e68f216481bfa90b9c761126a0b1
Ron Cohen <ron@opbeat.com>
2013-04-27 23:29:08 +0200

    Trying again.

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
