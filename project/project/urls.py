ba96855a717efd80ef58b1858cbd8cccc9216ae1
Ron Cohen <ron@opbeat.com>
2013-04-28 00:24:00 +0200

    Serve static files.

--- project/project/urls.py
+++ project/project/urls.py
@@ -8,5 +8,10 @@ urlpatterns = patterns(
     url(r'^', include('meetup.urls', namespace='meetup', app_name='meetup')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
+)
 
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+		'document_root': settings.STATIC_ROOT,
+    }),
 )
