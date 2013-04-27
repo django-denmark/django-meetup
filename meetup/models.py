e3c6a127fae90e27c0e288f2d4e338f89574286d
Víðir Valberg Guðmundsson <vidir.valberg@gmail.com>
2013-04-27 15:56:00 +0200

    Initial models

--- meetup/models.py
+++ meetup/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CreatedUpdatedModel(object):
+    created = models.DateTime(auto_now_add=True)
+    updated = models.DateTime(auto_now=True)
+
+
+class Talk(models.Model, CreatedUpdatedModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    speaker = models.ForeignKey(User)
+    meetup = models.ForeignKey('Meetup')
+    slides = models.URLField()
+
+
+class Meetup(models.Model, CreatedUpdatedModel):
+    when = models.DateTimeField()
+    venue = models.ForeignKey('Venue')
+
+
+class Venue(models.Model, CreatedUpdatedModel):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+class Speaker(models.Model, CreatedUpdatedModel):
+    user = models.ForeignKey(User, null=True)
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    url = models.URLField()
