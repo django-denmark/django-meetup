8f939aaae32fe8ec919d0465b87670827a7641d4
Ron Cohen <ron@opbeat.com>
2013-04-27 23:31:46 +0200

    okplzthx

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -23,6 +23,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+print sys.path
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
