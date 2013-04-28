eeb24db16eb4343b42564f74dd72fc25e6eaa4b7
Ron Cohen <ron@opbeat.com>
2013-04-28 14:19:20 +0200

    Mooore paths.

--- project/project/settings.py
+++ project/project/settings.py
@@ -7,7 +7,9 @@ TEMPLATE_DEBUG = DEBUG
 root = lambda x: os.path.abspath(os.path.join(os.path.dirname(__file__), x))
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
+print sys.path
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
