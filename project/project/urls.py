88cbfd7bbeb7093439cbf49de50c00ecee515944
Ron Cohen <ron@opbeat.com>
2013-04-28 03:15:33 +0200

    Added logout link.

--- project/project/urls.py
+++ project/project/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name="my_logout"),
     url(r'^', include('meetup.urls', namespace='meetup', app_name='meetup')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
