c7fdb52fc40cb711936cb7dc47689987a9596be8
Ron Cohen <ron@opbeat.com>
2013-04-28 15:55:36 +0200

    Change language to english.

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,7 +38,7 @@ TIME_ZONE = 'Europe/Copenhagen'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'da-dk'
+LANGUAGE_CODE = 'en-US'
 
 SITE_ID = 1
 
